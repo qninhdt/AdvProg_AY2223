1f27c048e4a78d35cdc43b891fd431cb6d1b884b
Quang Ninh <qndt123@gmail.com>
2023-02-28 07:48:23 +0700

    calculus

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -2,25 +2,25 @@
 #include <string>
 #include <cmath>
 
-using std::string;
 using std::cout;
 using std::endl;
 using std::stod;
-
+using std::string;
 
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: cosine of x
-***/
-double myCos(double x) 
+double myCos(double x)
 {
-    return 0.0;
+    double result = 1, tmp = 1;
+    int N = 39;
+    for (int i = 2; i <= N; i += 2)
+    {
+        tmp *= -x * x / (i * (i - 1));
+        result += tmp;
+    }
+    return result;
 }
 
 /***
@@ -31,9 +31,15 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    double result = x, tmp = x;
+    int N = 39;
+    for (int i = 3; i <= N; i += 2)
+    {
+        tmp *= -x * x / (i * (i - 1));
+        result += tmp;
+    }
+    return result;
 }
-
 
 /***
     Args:
@@ -41,12 +47,11 @@ double mySin(double x)
     Returns:
         double: square root of x
 ***/
-double mySqrt(double x) {
-    if (x < 0) {
-        cout << "Invalid argument" << endl;
-        exit(1);
-    }
-
-    
-    return 0;
+double mySqrt(double x)
+{
+    double result = 1;
+    int N = 20;
+    for (int i = 0; i < N; ++i)
+        result = (result + x / result) / 2;
+    return result;
 }
